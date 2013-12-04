aebe887a2bc55afff190b3ea278889a48cf38e84
Jenny Baran <jenny.baran@gmail.com>
2013-12-03 19:04:02 -0500

    hw36
    + cleaning out tilde files

--- SuperArray.java
+++ SuperArray.java
@@ -20,8 +20,8 @@ public class SuperArray {
     //initializes 10-item array
     public SuperArray() { 
 	_data = new int[10];
-	_size = 10;
-	_lastPost = -1;
+	_size = 0;
+	_lastPos = 0;
     }
 
 
@@ -43,27 +43,30 @@ public class SuperArray {
 
     //double capacity of this instance of SuperArray 
     private void expand() { 
-	// *** YOUR IMPLEMENTATION HERE *** 
+	int[] nData = new int[ _data.length * 2 ];
+	for ( int i = 0; i < _data.length; i++ ) {
+	    nData[i] = _data[i];
+	}
+	_data = nData;
     }
 
 
     //accessor method -- return value at specified index
     public int get( int index ) {
-	// *** YOUR IMPLEMENTATION HERE *** 
 	return _data[index];
     }
 
 
     //mutator method -- set index to newVal, return old value at index
     public int set( int index, int newVal ) {
-	// *** YOUR IMPLEMENTATION HERE *** 
-	return -1; 
+	int retInt = _data[index];
+	_data[index] = newVal;
+	return retInt; 
     }
 
     //main method for testing
     public static void main( String[] args ) {
 
-	/*===========================================
 	SuperArray curtis = new SuperArray();
 	System.out.println( "Printing empty SuperArray curtis..." );
 	System.out.println( curtis );
@@ -73,9 +76,21 @@ public class SuperArray {
 	    curtis._size++;
 	}
 
-	System.out.println("Printing populated SuperArray curtis...");
+	System.out.println("\nPrinting populated SuperArray curtis...");
 	System.out.println(curtis);
-	===========================================*/
+
+	curtis.expand();
+
+	for ( int i = curtis._size; i < curtis._data.length; i++ ) {
+	    curtis.set( i, i * 2 );
+	    curtis._size++;
+	}
+
+	System.out.println("\nPrinting expanded & populated SuperArray curtis...");
+	System.out.println(curtis);
+
+	System.out.println("\nPrinting value at index 5 in SuperArray curtis...");
+	System.out.println( curtis.get(5) ); //should return 10
 
     }
 
