08a1b1e86d324096695683b44f1d0fcc46110e2e
Jenny Baran <jenny.baran@gmail.com>
2013-12-03 19:45:31 -0500

    hw37

--- SuperArray.java
+++ SuperArray.java
@@ -1,70 +1,127 @@
 // JENNY BARAN
 // pd 8
-// HW36
-// 2013-12-02
+// HW37
+// 2013-12-03
 
 /*==================================================
-  class SuperArray
-  Wrapper class for array. Facilitates resizing, 
-  getting and setting element values.
+  class SuperArray version 2.0
+  Wrapper class for array. Facilitates 
+  resizing 
+  expansion 
+  read/write capability on elements
+  adding an element to end of array
+  adding an element at specified index
+  removing an element at specified index
   ==================================================*/
 
 public class SuperArray {
 
-    private int[] _data;
-    private int _lastPos;
-    private int _size;
-
+    private int[] _data;  //underlying container structure
+    private int _lastPos; //marker for last meaningful value
+    private int _size;    //number of meaingful values
 
     //default constructor
     //initializes 10-item array
     public SuperArray() { 
 	_data = new int[10];
-	_size = 0;
-	_lastPos = 0;
+	_lastPos = -1;
+	_size = 0;	
     }
-
 
     //output array in [a,b,c] format
     //eg, for int[] a = {1,2,3} ...
     //toString() -> "[1,2,3]"
     public String toString() { 
-	String output = "[";
-
-	for ( int x : _data ) {
-	    output += x + ",";
+	String foo = "[";
+	for( int i = 0; i < _data.length; i++ ) {
+	    foo += _data[i] + ",";
 	}
-	
-	output = output.substring( 0, output.length()-1 ) + "]";
-
-	return output;
+	if ( foo.length() > 1 )
+	    //shave off trailing comma
+	    foo = foo.substring( 0, foo.length()-1 );
+	foo += "]";
+	return foo;
     }
-
 
     //double capacity of this instance of SuperArray 
     private void expand() { 
-	int[] nData = new int[ _data.length * 2 ];
-	for ( int i = 0; i < _data.length; i++ ) {
-	    nData[i] = _data[i];
-	}
-	_data = nData;
+	int[] temp = new int[ _data.length * 2 ];
+	for( int i = 0; i < _data.length; i++ )
+	    temp[i] = _data[i];
+	_data = temp;
     }
-
 
     //accessor method -- return value at specified index
     public int get( int index ) {
 	return _data[index];
     }
 
-
     //mutator method -- set index to newVal, return old value at index
     public int set( int index, int newVal ) {
-	int retInt = _data[index];
+	int temp = _data[index];
 	_data[index] = newVal;
-	return retInt; 
+	return temp;
     }
 
-    //main method for testing
+
+    //adds an item after the last item
+    public void add( int newVal ) { 
+	if ( _lastPos == _data.length - 1 ) {
+	    this.expand();
+	}
+	_data[ _lastPos + 1 ] = newVal;
+	_lastPos++;
+	_size = _lastPos + 1;
+    }
+
+
+    //inserts an item at index    
+    public void add( int index, int newVal ) { 
+	if ( index > _data.length - 1) {
+	    this.expand();
+	}
+
+	if ( index <= _lastPos ) {
+	    if ( (_lastPos + 1) >= _data.length ) {
+		this.expand();
+	    }
+	    int temp;
+	    int override = newVal;
+	    for ( int i = index; i < _data.length - 1; i++ ) {
+		temp = _data[i];
+		_data[i] = override;
+		override = temp;
+	    }
+	    _lastPos++;
+	}
+	else { //else of if (index <= _lastpos)
+	    _data[index] = newVal;
+	    _lastPos = index;
+	}
+	_size = _lastPos + 1;
+    }
+
+
+    //removes the item at index
+    //shifts elements left to fill in newly-empted slot
+    public void remove( int index ) { 
+	if ( index <= _lastPos ) {
+	    for ( int i = index; i < _data.length - 1; i++ ) {
+		_data[i] = _data[i+1];
+	    }
+	    _data[_lastPos] = 0;
+	    _lastPos--;
+	    _size--;
+	}
+    }
+
+
+    //return number of meaningful items in _data
+    public int size() {
+	return _size;
+    }
+
+
     public static void main( String[] args ) {
 
 	SuperArray curtis = new SuperArray();
@@ -73,25 +130,51 @@ public class SuperArray {
 
 	for( int i = 0; i < curtis._data.length; i++ ) {
 	    curtis.set( i, i * 2 );
-	    curtis._size++;
 	}
 
-	System.out.println("\nPrinting populated SuperArray curtis...");
+	System.out.println("Printing populated SuperArray curtis...");
 	System.out.println(curtis);
 
-	curtis.expand();
-
-	for ( int i = curtis._size; i < curtis._data.length; i++ ) {
-	    curtis.set( i, i * 2 );
-	    curtis._size++;
+	for( int i = 0; i < 3; i++ ) {
+	    curtis.expand();
+	    System.out.println("Printing expanded SuperArray curtis...");
+	    System.out.println(curtis);
+	    System.out.println("new length of underlying array: " 
+			       + curtis._data.length );
 	}
 
-	System.out.println("\nPrinting expanded & populated SuperArray curtis...");
-	System.out.println(curtis);
+	System.out.println( curtis.get(6) );
+	
+	SuperArray mayfield = new SuperArray();
+	System.out.println("Printing empty SuperArray mayfield...");
+	System.out.println(mayfield);
 
-	System.out.println("\nPrinting value at index 5 in SuperArray curtis...");
-	System.out.println( curtis.get(5) ); //should return 10
+	mayfield.add(5);
+	mayfield.add(4);
+	mayfield.add(3);
+	mayfield.add(2);
+	mayfield.add(1);
 
-    }
+	System.out.println("Printing populated SuperArray mayfield...");
+	System.out.println(mayfield);
+
+	mayfield.remove(3);
+	System.out.println("Printing SuperArray mayfield post-remove...");
+	System.out.println(mayfield);
+	mayfield.remove(3);
+	System.out.println("Printing SuperArray mayfield post-remove...");
+	System.out.println(mayfield);
+
+	mayfield.add(3,99);
+	System.out.println("Printing SuperArray mayfield post-insert...");
+	System.out.println(mayfield);
+	mayfield.add(2,88);
+	System.out.println("Printing SuperArray mayfield post-insert...");
+	System.out.println(mayfield);
+	mayfield.add(1,77);
+	System.out.println("Printing SuperArray mayfield post-insert...");
+	System.out.println(mayfield);
+
+    }//end main()
 
 }//end class SuperArray
