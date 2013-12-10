fdbd8edea9f5f1846bea2fe0574c8f0a580d8257
Jenny Baran <jenny.baran@gmail.com>
2013-12-09 20:23:25 -0500

    work from class on hw40

---
+++ OrderedArrayList.java
@@ -0,0 +1,107 @@
+// JENNY BARAN
+// pd 8
+// HW40
+// 2013-12-09
+
+/*============================================
+   class OrderedArrayList
+   Wrapper class for ArrayList.
+   Imposes the restriction that stored items 
+   must remain sorted in ascending order
+   ============================================*/
+
+//ArrayList's implementation is in the java.util package
+import java.util.ArrayList;
+
+
+public class OrderedArrayList {
+
+    // instance of class ArrayList, holding objects of type Comparable 
+    // (ie, instances of a class that implements interface Comparable)
+    private ArrayList<Comparable> _data;
+
+
+    // default constructor initializes instance variable _data
+    public OrderedArrayList() {
+	_data = new ArrayList();
+    }
+
+
+    public String toString() { 
+	String foo = "[";
+	for( int i = 0; i < this.size()-1; i++ ) {
+	    foo += _data.get(i) + ",";
+	}
+	if ( foo.length() > 1 )
+	    //shave off trailing comma
+	    foo = foo.substring( 0, foo.length()-1 );
+	foo += "]";
+	return foo;
+    }
+
+
+    public Comparable remove( int index ) { 
+	return _data.remove(index);
+    }
+
+
+    public int size() { 
+	return _data.size();
+    }
+
+    
+    public Comparable get( int index ) { 
+	return _data.get(index);
+    }
+
+
+    // addLinear takes as input any comparable object 
+    // (i.e., any object of a class implementing interface Comparable)
+    // inserts newVal at the appropriate index
+    // maintains ascending order of elements
+    // uses a linear search to find appropriate index
+    public void addLinear(Comparable newVal) { 
+	if (_data.get(_data.length-1) > newVal) {
+	    for ( int i = 0; i < _data.size(); i++) {
+		if (_data.get(i) >= newVal) {
+		    add(i, newVal);
+		}
+	    }
+	}
+	else {
+	    add(newVal);
+	}
+    }
+
+
+    // addBinary takes as input any comparable object 
+    // (i.e., any object of a class implementing interface Comparable)
+    // inserts newVal at the appropriate index
+    // maintains ascending order of elements
+    // uses a binary search to find appropriate index
+    public void addBinary(Comparable newVal) { 
+	// *** YOUR IMPLEMENTATION HERE ***
+    }	
+
+
+    // main method solely for testing purposes
+    public static void main( String[] args ) {
+
+	OrderedArrayList Franz = new OrderedArrayList();
+
+	// testing linear search
+	for( int i = 0; i < 15; i++ )
+	    Franz.addLinear( (int)( 50 * Math.random() ) );
+	System.out.println( Franz );
+
+	/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+	// testing binary search
+	Franz = new OrderedArrayList();
+	for( int i = 0; i < 15; i++ ) 
+	    Franz.addBinary( (int)( 50 * Math.random() ) );
+	System.out.println( Franz );
+	   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    }
+
+}//end class OrderedArrayList
---
+++ OrderedArrayList.java~
@@ -0,0 +1,88 @@
+/*============================================
+   class OrderedArrayList
+   Wrapper class for ArrayList.
+   Imposes the restriction that stored items 
+   must remain sorted in ascending order
+   ============================================*/
+
+//ArrayList's implementation is in the java.util package
+import java.util.ArrayList;
+
+
+public class OrderedArrayList {
+
+    // instance of class ArrayList, holding objects of type Comparable 
+    // (ie, instances of a class that implements interface Comparable)
+    private ArrayList<Comparable> _data;
+
+
+    // default constructor initializes instance variable _data
+    public OrderedArrayList() {
+	// *** YOUR IMPLEMENTATION HERE ***
+    }
+
+
+    public String toString() { 
+	// *** YOUR IMPLEMENTATION HERE ***
+	return ""; //placeholder
+    }
+
+
+    public Comparable remove( int index ) { 
+	// *** YOUR IMPLEMENTATION HERE ***
+	return ""; //placeholder 
+    }
+
+
+    public int size() { 
+	// *** YOUR IMPLEMENTATION HERE ***
+	return -1; //placeholder 
+    }
+
+    
+    public Comparable get( int index ) { 
+	// *** YOUR IMPLEMENTATION HERE ***
+	return ""; //placeholder  
+    }
+
+
+    // addLinear takes as input any comparable object 
+    // (i.e., any object of a class implementing interface Comparable)
+    // inserts newVal at the appropriate index
+    // maintains ascending order of elements
+    // uses a linear search to find appropriate index
+    public void addLinear(Comparable newVal) { 
+	// *** YOUR IMPLEMENTATION HERE ***
+    }
+
+
+    // addBinary takes as input any comparable object 
+    // (i.e., any object of a class implementing interface Comparable)
+    // inserts newVal at the appropriate index
+    // maintains ascending order of elements
+    // uses a binary search to find appropriate index
+    public void addBinary(Comparable newVal) { 
+	// *** YOUR IMPLEMENTATION HERE ***
+    }	
+
+
+    // main method solely for testing purposes
+    public static void main( String[] args ) {
+
+	/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	OrderedArrayList Franz = new OrderedArrayList();
+
+	// testing linear search
+	for( int i = 0; i < 15; i++ )
+	    Franz.addLinear( (int)( 50 * Math.random() ) );
+	System.out.println( Franz );
+
+	// testing binary search
+	Franz = new OrderedArrayList();
+	for( int i = 0; i < 15; i++ ) 
+	    Franz.addBinary( (int)( 50 * Math.random() ) );
+	System.out.println( Franz );
+	   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    }
+
+}//end class OrderedArrayList
