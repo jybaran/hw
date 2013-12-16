9db05a1416dc0888dae75f920c8ebc65692b3d64
Jenny Baran <jenny.baran@gmail.com>
2013-12-16 17:10:42 -0500

    classwork 2013-12-16

---
+++ Concentration/Concentration.java
@@ -0,0 +1,35 @@
+/*========================================
+class Concentration
+Facilitates playing game of Concentration,
+aka Memory.
+========================================*/
+
+import java.util.ArrayList;
+import cs1.Keyboard;
+
+public class Concentration {
+
+    //instance variables
+    //storage for 4x4 grid of Tiles. _board.size() == 16	
+    private ArrayList<Tile> _board; 
+
+    //count of Tiles with faces visible
+    private int _numberFaceUp;  
+
+    //list of Strings used for Tile vals
+    private WordList _list;
+
+    private static int _numRows = 4;
+
+
+    // *** INSERT YOUR CONSTRUCTOR AND METHODS HERE ***
+
+
+    //DO NOT MODIFY main()
+    public static void main( String[] args ) {
+	Concentration game = new Concentration();
+	game.play();
+    }
+
+}//end class Concentration
+
